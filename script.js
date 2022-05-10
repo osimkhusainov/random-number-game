7cf5c3607656e45d5d1b4fdbcccc8d9c2d0d4e1e
Osim Khusainov <keedice@gmail.com>
2022-05-10 15:17:22 -0400

    refactoring

---
+++ script.js
@@ -0,0 +1,52 @@
+"use strict";
+const checkBtn = document.querySelector(".btn.check");
+const numInput = document.querySelector(".guess");
+const message = document.querySelector(".message");
+const score = document.querySelector(".score");
+const numberScreen = document.querySelector(".number");
+const btnAgain = document.querySelector(".btn.again");
+const highscore = document.querySelector(".highscore");
+
+const getRandomName = (min, max) => {
+  return Math.trunc(Math.random() * (max - min + 1) + min);
+};
+let myNumber = getRandomName(1, 20);
+console.log(myNumber);
+const decreaseScore = () =>
+  score.textContent < 1
+    ? (message.textContent = "You lose! Try again")
+    : score.textContent--;
+
+checkBtn.addEventListener("click", () => {
+  const input = +numInput.value;
+  if (!input) {
+    message.textContent = "Enter number";
+    decreaseScore();
+    console.log(message.textContent);
+  } else if (input === myNumber) {
+    message.textContent = "You won!";
+    document.querySelector("body").style.backgroundColor = "green";
+    numberScreen.textContent = myNumber;
+    numInput.disabled = checkBtn.disabled = true;
+    score.textContent > highscore.textContent
+      ? (highscore.textContent = score.textContent)
+      : highscore.textContent;
+    myNumber = getRandomName(1, 20);
+  } else if (input < myNumber) {
+    message.textContent = "Too low!";
+    decreaseScore();
+  } else if (input > myNumber) {
+    message.textContent = "Too high!";
+    decreaseScore();
+  }
+});
+
+btnAgain.addEventListener("click", () => {
+  numberScreen.textContent = "?";
+  numInput.disabled = false;
+  checkBtn.disabled = false;
+  message.textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "";
+  score.textContent = 20;
+  numInput.value = "";
+});
